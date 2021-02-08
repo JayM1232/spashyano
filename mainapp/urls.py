d6735928b1f57e5601e4764df753d1d48ab9f7e5
tanajim <69978761+tanajim@users.noreply.github.com>
2021-02-08 14:35:45 +0530

    Update urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -23,8 +23,8 @@ from django.conf.urls import url
 urlpatterns = [
     path('',include('firstapp.urls')),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
+#     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+#     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
